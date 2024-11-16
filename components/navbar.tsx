102fdd71fab9f60aae371b6a61b9cf68c041f492
Leonfarhan <leonfarhan404@gmail.com>
2024-11-16 16:22:24 +0700

    fix: bugs

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,7 +7,7 @@ import { ThemeToggle } from "./theme-toggle";
 export function Navbar() {
   return (
     <header className="fixed top-0 z-50 w-full border-b bg-background/80 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 justify-between">
+      <div className="max-w-screen-xl mx-auto flex h-16 justify-between px-4 md:px-8">
         <div className="flex items-center space-x-3">
           <Link href="/" className="text-xl font-semibold">
             MintMate
